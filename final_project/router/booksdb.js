59e489dd2e1d0bb05dc81242c9c5d2aa25523af1
Huy Le <huyyyle@gmail.com>
2024-06-30 17:19:28 -0400

    submission

--- final_project/router/booksdb.js
+++ final_project/router/booksdb.js
@@ -1,14 +1,14 @@
 let books = {
-      1: {"author": "Chinua Achebe","title": "Things Fall Apart", "reviews": {} },
-      2: {"author": "Hans Christian Andersen","title": "Fairy tales", "reviews": {} },
-      3: {"author": "Dante Alighieri","title": "The Divine Comedy", "reviews": {} },
-      4: {"author": "Unknown","title": "The Epic Of Gilgamesh", "reviews": {} },
-      5: {"author": "Unknown","title": "The Book Of Job", "reviews": {} },
-      6: {"author": "Unknown","title": "One Thousand and One Nights", "reviews": {} },
-      7: {"author": "Unknown","title": "Nj\u00e1l's Saga", "reviews": {} },
-      8: {"author": "Jane Austen","title": "Pride and Prejudice", "reviews": {} },
-      9: {"author": "Honor\u00e9 de Balzac","title": "Le P\u00e8re Goriot", "reviews": {} },
-      10: {"author": "Samuel Beckett","title": "Molloy, Malone Dies, The Unnamable, the trilogy", "reviews": {} }
+    1: {"author": "Chinua Achebe","title": "Things Fall Apart", "reviews": {"review1": "okay book"},"isbn": "1", },
+    2: {"author": "Hans Christian Andersen","title": "Fairy tales", "reviews": {"review1": "Classic stories for all ages."}, "isbn": "2" },
+    3: {"author": "Dante Alighieri","title": "The Divine Comedy", "reviews": {"review1": "A masterpiece of literature."}, "isbn": "3" },
+    4: {"author": "Unknown","title": "The Epic Of Gilgamesh", "reviews": {"review1": "Fascinating ancient epic."}, "isbn": "4" },
+    5: {"author": "Unknown","title": "The Book Of Job", "reviews": {"review1": "A profound and challenging read."}, "isbn": "5" },
+    6: {"author": "Unknown","title": "One Thousand and One Nights", "reviews": {"review1": "A delightful collection of stories."}, "isbn": "6" },
+    7: {"author": "Unknown","title": "Nj\u00e1l's Saga", "reviews": {"review1": "A gripping medieval saga."}, "isbn": "7" },
+    8: {"author": "Jane Austen","title": "Pride and Prejudice", "reviews": {"review1": "A timeless classic."}, "isbn": "8" },
+    9: {"author": "Honor\u00e9 de Balzac","title": "Le P\u00e8re Goriot", "reviews": {"review1": "A heartbreaking tale of love and loss."}, "isbn": "9" },
+    10: {"author": "Samuel Beckett","title": "Molloy, Malone Dies, The Unnamable, the trilogy", "reviews": {"review1": "A challenging and experimental work."}, "isbn": "10" }
 }
 
 module.exports=books;
